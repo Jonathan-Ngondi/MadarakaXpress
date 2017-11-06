72a7ee5d04ec380c5fd90cf5d8beb75bfb1557ff
DESKTOP-RAAFD5A\adelinetush <adelinetush@gmail.com>
2017-11-06 12:12:58 +0300

    Basic buss class

---
+++ models/bus.py
@@ -0,0 +1,20 @@
+class Bus: 
+    def __init__(self, name, destination):
+        self.busname = name;
+        self.destination = destination
+        self.capacity = 30
+
+    def get_busname(self):
+        return self.busname
+
+    def get_buscapacity(self):
+        return self.capacity
+
+    def get_busdestination(self):
+        return self.destination
+
+
+
+
+
+
