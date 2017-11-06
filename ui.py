75b789b176582edc4001f6b31cf659998c475c22
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2017-11-06 11:58:54 +0300

    Add function stubs for the commandline interface

---
+++ ui.py
@@ -0,0 +1,33 @@
+from docopt import docopt
+ 
+
+
+def register_user():
+    "user registers here"
+    pass
+
+def book_user():
+    "user books here"
+    
+    pass
+
+def cancel_booking():
+    "Cancels users bookings "
+    pass       
+
+def retrieve_user():
+    "retrieves user info from storage"
+
+     pass
+
+def update_user():
+    "updates bookings"
+    pass
+    
+    if __name__ =="__main__":
+        
+
+
+
+
+
